952de6677f8d07467cfb3707a675a05c20b2325b
trussugavi01 <trussugavi@chuksigboegwu.com>
2025-12-12 23:37:28 +0100

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,6 @@
 import './bootstrap';
-import Alpine from 'alpinejs';
 import Sortable from 'sortablejs';
 import Chart from 'chart.js/auto';
 
-// Make Alpine available globally
-window.Alpine = Alpine;
 window.Sortable = Sortable;
 window.Chart = Chart;
-
-// Start Alpine
-Alpine.start();
